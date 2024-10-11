ba6eeddef98df3682000968a9142fcd831d00b47
vale <valentinpoblete99@hotmail.com>
2024-10-11 13:01:55 -0300

    Modificaciones clases turnero y CentroDeSalud

--- src/main/java/tpintegrador/proyecto/CentroDeSalud.java
+++ src/main/java/tpintegrador/proyecto/CentroDeSalud.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tpintegrador.proyecto;
+import java.util.ArrayList;
 
 /**
  *
@@ -13,20 +14,49 @@ public class CentroDeSalud {
     //Atributos
     private String ubicación;
     private int contacto;
+    private static ArrayList<Especialidades> listaEspecialidades = new ArrayList<>();;
+    private static ArrayList<Medico> listaMedicos = new ArrayList<>();;
     
-    //Constructor
-    public CentroDeSalud(String ubicación, int contacto){
+        //Constructor
+    public CentroDeSalud(String ubicacion, int contacto){
         this.ubicación = ubicación;
-        this.contacto = contacto;
-       
+        this.contacto = contacto; 
+    }    
+    public void cargarListas(){
+        // Inicializar especialidades
+        Especialidades cardiologia = new Especialidades("Cardiología");
+        Especialidades pediatria = new Especialidades("Pediatría");
+        Especialidades dermatologia = new Especialidades("Dermatología");
+        
+        listaEspecialidades.add(cardiologia);
+        listaEspecialidades.add(pediatria);
+        listaEspecialidades.add(dermatologia);
+        // Inicializar Medicos
+        Medico medico1 = new Medico(cardiologia, "12345", "9:00-17:00", "Dr. Juan Pérez", "Pérez", 12345678, 40);
+        Medico medico2 = new Medico(dermatologia, "67890", "9:00-17:00", "Dra. Lautaro Sanchez", "Sanchez", 87654321, 35); 
+        Medico medico3 = new Medico(pediatria, "67890", "9:00-17:00", "Dra. Monica Marquez", "Gómez", 87654321, 31);
+        
+        listaMedicos.add(medico1);
+        listaMedicos.add(medico2);
+        listaMedicos.add(medico3);
     }
     
+    //Metodos
+    public ArrayList<Medico> obtenerMedicos() {
+        return listaMedicos;
+    }
+
+    public ArrayList<Especialidades> obtenerEspecialidades() {
+        return  listaEspecialidades;
+    }
+      
     //Metodos gets y sets
+
     public String getUbicación() {
-        return ubicación;
+      return ubicación;
     }
     public void setUbicación(String ubicación) {
-        this.ubicación = ubicación;
+      this.ubicación = ubicación;
     }
     public int getContacto() {
         return contacto;
@@ -34,7 +64,18 @@ public class CentroDeSalud {
     public void setContacto(int contacto) {
         this.contacto = contacto;
     }
-    
+    public ArrayList<Medico> getListaMedicos() {
+        return listaMedicos;
+    }
+    public void setListaMedicos(ArrayList<Medico> listaMedicos) {
+        this.listaMedicos = listaMedicos;
+    }
+    public ArrayList<Especialidades> getListaEspecialidades() {
+        return listaEspecialidades;
+    }
+    public void setListaEspecialidades(ArrayList<Especialidades> listaEspecialidades) {
+        this.listaEspecialidades = listaEspecialidades;
+    }
     //Metodos
     public void recibirPersonas() {
         //Agregar este metodo a el UML
--- src/main/java/tpintegrador/proyecto/Turnero.java
+++ src/main/java/tpintegrador/proyecto/Turnero.java
@@ -8,12 +8,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Alert;
 import java.time.LocalDate;
+import java.util.Iterator;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
-import java.util.ArrayList;
 
 /**
  *
@@ -22,8 +22,6 @@ import java.util.ArrayList;
 public class Turnero {
 
     //Atributos
-    private ArrayList<Medico> listaMedicos;
-    private ArrayList<Especialidades> listaEspecialidades;
     @FXML
     private ComboBox<Especialidades> comboEspecialidades; // ComboBox para seleccionar especialidad
     @FXML
@@ -34,39 +32,11 @@ public class Turnero {
     private Button btnMostrarMedicos; // Botón para mostrar médicos
     @FXML
     private Button btnAgendarTurno; // Botón para agendar el turno
-
-    public Turnero() {
-        listaMedicos = new ArrayList<>();
-        listaEspecialidades = new ArrayList<>();
-
-        //Llenar listas de médicos y especialidades
-        cargarListas();
-    }
-
-    // Método para llenar listas 
-    private void cargarListas() {
-        Especialidades cardiologia = new Especialidades("Cardiología");
-        Especialidades traumatologia = new Especialidades("Traumatologia");
-        Especialidades pediatria = new Especialidades("Pediatría");
-
-        listaEspecialidades.add(cardiologia);
-        listaEspecialidades.add(pediatria);
-        listaEspecialidades.add(traumatologia);
-
-        listaMedicos.add(new Medico(cardiologia, "12345", "9:00-17:00", "Dr. Juan Pérez", "Pérez", 12345678, 40));
-        listaMedicos.add(new Medico(cardiologia, "12345", "9:00-17:00", "Dr. Fernando Torres", "Pérez", 12345678, 43));
-        listaMedicos.add(new Medico(cardiologia, "12345", "9:00-17:00", "Dr. Carlos Arias", "Pérez", 12345678, 32));
-        listaMedicos.add(new Medico(traumatologia, "67890", "9:00-17:00", "Dra. Alejandro Gonzalez", "Gómez", 87654321, 30));
-        listaMedicos.add(new Medico(traumatologia, "67890", "9:00-17:00", "Dra. Lautaro Sanchez", "Sanchez", 87654321, 35));
-        listaMedicos.add(new Medico(traumatologia, "67890", "9:00-17:00", "Dra. Matias Costela", "Costela", 87654321, 33));
-        listaMedicos.add(new Medico(pediatria, "67890", "9:00-17:00", "Dra. Federica Gómez", "Gómez", 87654321, 35));
-        listaMedicos.add(new Medico(pediatria, "67890", "9:00-17:00", "Dra. Maria Gutierrez", "Gómez", 87654321, 39));
-        listaMedicos.add(new Medico(pediatria, "67890", "9:00-17:00", "Dra. Monica Marquez", "Gómez", 87654321, 31));
-
-    }
-
+    
     // Método para cargar y mostrar la interfaz gráfica
     public void interfazGrafica(Stage interfaz) throws Exception {
+        CentroDeSalud cs = new CentroDeSalud("sadasd", 0);
+        cs.cargarListas();
         // Cargar el archivo FXML
         FXMLLoader loader = new FXMLLoader(getClass().getResource("turnero.fxml"));
         Parent root = loader.load();
@@ -80,33 +50,35 @@ public class Turnero {
         interfaz.show();
 
         // Llenar el ComboBox de especialidades
-        turneroController.comboEspecialidades.getItems().addAll(turneroController.listaEspecialidades);
+        turneroController.comboEspecialidades.getItems().addAll(cs.getListaEspecialidades());
 
         // Manejar el evento de selección de especialidad
         turneroController.btnMostrarMedicos.setOnAction(event -> turneroController.mostrarMedicos());
 
         // Manejar el evento de agendar turno
         turneroController.btnAgendarTurno.setOnAction(event -> turneroController.agendarTurno());
-
     }
 
     // Método para mostrar médicos de la especialidad seleccionada
     @FXML
     private void mostrarMedicos() {
+        CentroDeSalud cs = new CentroDeSalud("asdasd", 0);
+        cs.cargarListas();
         Especialidades especialidadSeleccionada = comboEspecialidades.getValue();
         listViewMedicos.getItems().clear(); // Limpiar la lista antes de mostrar
-
         if (especialidadSeleccionada != null) {
-            for (Medico medico : listaMedicos) {
-                if (medico.getEspecialidad().equals(especialidadSeleccionada)) {
-                    listViewMedicos.getItems().add(medico); // Agregar médicos a la ListView
-                }
+        Iterator<Medico> it = cs.getListaMedicos().iterator();
+        while (it.hasNext()) {
+            Medico medico = it.next();
+            if (medico.getEspecialidad().equals(especialidadSeleccionada)) {
+                listViewMedicos.getItems().add(medico); // Agregar médicos a la ListView
             }
-        } else {
-            showAlert("Error", "Por favor, seleccione una especialidad.");
         }
+    } else {
+        showAlert("Error", "Por favor, seleccione una especialidad.");
     }
-
+        
+   }
     // Método para agendar un turno
     @FXML
     private void agendarTurno() {
