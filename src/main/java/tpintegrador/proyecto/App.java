e886616f9b90b8c9bf2c16d2f71a4684a41b4b64
LaureanoPetri <laureanopetri@gmail.com>
2024-10-21 17:44:31 -0300

    conexion de sqlite en clase GestionTurno

--- src/main/java/tpintegrador/proyecto/App.java
+++ src/main/java/tpintegrador/proyecto/App.java
@@ -2,7 +2,7 @@ package tpintegrador.proyecto;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import java.sql.*;
+//import java.sql.*;
 
 
 public class App extends Application {
@@ -15,30 +15,8 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        
-        Connection conn = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:test01.sqlite");
-
-            if (conn != null)
-		System.out.println("Conexión a base de datos ... Ok");
-            else
-                System.out.println("Conexión a base de datos: problemas!");
-
-           conn.close();
-
-        }
-        catch (SQLException a) {
-            System.out.println(a);
-        }
-        catch (ClassNotFoundException ex) {
-            System.out.println(ex);
-        }
-        catch (Exception ex) {
-            System.out.println(ex);
-        }
-        
+         GestionTurnos dbTurnos = new GestionTurnos();
+         dbTurnos.conexion();
         launch(args);
     }
   }
--- src/main/java/tpintegrador/proyecto/GestionTurnos.java
+++ src/main/java/tpintegrador/proyecto/GestionTurnos.java
@@ -3,13 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tpintegrador.proyecto;
-
+import java.sql.*;
 /**
  *
  * @author laure
  */
 public class GestionTurnos {
     
+    public void conexion() {
+        Connection conn = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:turnos.sqlite");
+
+            if (conn != null)
+		System.out.println("Conexión a base de datos ... Ok");
+            else
+                System.out.println("Conexión a base de datos: problemas!");
+
+           conn.close();
+
+        }
+        catch (SQLException a) {
+            System.out.println(a);
+        }
+        catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
+}
     //Posible base de datos
     public void agregarTurno(Paciente paciente){
         
