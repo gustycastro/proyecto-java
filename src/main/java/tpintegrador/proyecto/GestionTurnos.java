7fbb1e86d6f7468b45045cbdfbea003f917715d0
LaureanoPetri <laureanopetri@gmail.com>
2024-10-22 13:00:12 -0300

    cree el metodos de crearTabla y el metodo insertar pacientes para guardarlos en la base de datos

--- src/main/java/tpintegrador/proyecto/GestionTurnos.java
+++ src/main/java/tpintegrador/proyecto/GestionTurnos.java
@@ -12,15 +12,19 @@ public class GestionTurnos {
     
     public void conexion() {
         Connection conn = null;
+        Statement stmt = null;
+
         try {
             Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection("jdbc:sqlite:turnos.sqlite");
 
-            if (conn != null)
+            if (conn != null){
 		System.out.println("Conexión a base de datos ... Ok");
-            else
+                
+                crearTabla(conn, stmt);
+            }else{
                 System.out.println("Conexión a base de datos: problemas!");
-
+            }
            conn.close();
 
         }
@@ -33,7 +37,26 @@ public class GestionTurnos {
         catch (Exception ex) {
             System.out.println(ex);
         }
-}
+    }
+    public void crearTabla(Connection conn, Statement stmt){
+        try{
+        stmt = conn.createStatement();
+                String sql = "CREATE TABLE TablaPacientes " +
+                        "(ID paciente     INT     NOT NULL," +
+                        " nombre          TEXT    NOT NULL, " + 
+                        " edad            INT     NOT NULL, " + 
+                        " fecha           DATE     NOT NULL, " + 
+                        " DNI             INT     NOT NULL)"; 
+                stmt.executeUpdate(sql);
+                stmt.close();
+                conn.close();
+        } catch ( Exception e ) {
+         System.err.println("error en Tabla");
+         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+         System.exit(0);
+        }
+      System.out.println("Tabla creada correctamente");
+    }
     //Posible base de datos
     public void agregarTurno(Paciente paciente){
         
