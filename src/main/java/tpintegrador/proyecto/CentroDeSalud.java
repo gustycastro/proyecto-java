f98029f21434dd9cf116160f05dc6754eaeb5d6f
gustycastro <gusty12rd@gmail.com>
2024-10-23 21:56:34 -0300

    se elimino el condicional del metodo cargarListas

--- src/main/java/tpintegrador/proyecto/CentroDeSalud.java
+++ src/main/java/tpintegrador/proyecto/CentroDeSalud.java
@@ -19,7 +19,6 @@ public class CentroDeSalud {
     private static ArrayList<Especialidades> listaEspecialidades = new ArrayList<>();
     private static ArrayList<Medico> listaMedicos = new ArrayList<>();
     private static ArrayList<ObraSocial> listaObraSocial = new ArrayList<>();
-    private boolean listasCargadas = false;
 
     //Constructor
     public CentroDeSalud(String ubicacion, int contacto) {
@@ -28,37 +27,35 @@ public class CentroDeSalud {
     }
 
     public void cargarListas() {
-        if (!listasCargadas) {
-            listaMedicos.clear();        // Limpiar antes de agregar
 
-            // Inicializar Especialidades usando un array y un bucle
-            String[] nombresEspecialidades = {"Cardiología", "Pediatría", "Dermatología", "Traumatologia", "Oftalmología"};
-            for (String nombre : nombresEspecialidades) {
-                listaEspecialidades.add(new Especialidades(nombre));
-            }
-
-            // Inicializar Médicos
-            Medico medico1 = new Medico(listaEspecialidades.get(0), 12345, "9:00-17:00", "Dr. Juan Pérez", "Pérez", 12345678, 40);
-            Medico medico2 = new Medico(listaEspecialidades.get(1), 67890, "9:00-17:00", "Dra. Maria Sanchez", "Sanchez", 87654321, 35);
-            Medico medico3 = new Medico(listaEspecialidades.get(2), 67890, "9:00-17:00", "Dra. Monica Marquez", "Gómez", 87654321, 31);
-            Medico medico4 = new Medico(listaEspecialidades.get(3), 67890, "9:00-17:00", "Dr. Carlos Arias", "Gómez", 87654321, 31);
-            Medico medico5 = new Medico(listaEspecialidades.get(0), 12345, "9:00-17:00", "Dr. Alfredo Pucci", "Pérez", 12345678, 40);
-            Medico medico6 = new Medico(listaEspecialidades.get(1), 67890, "9:00-17:00", "Dr. Alberto Perez", "Sanchez", 87654321, 35);
-            Medico medico7 = new Medico(listaEspecialidades.get(2), 67890, "9:00-17:00", "Dra. Federica Gomez", "Gómez", 87654321, 31);
-            Medico medico8 = new Medico(listaEspecialidades.get(3), 67890, "9:00-17:00", "Dr. Matias Gonzalez", "Gómez", 87654321, 31);
-            Medico medico9 = new Medico(listaEspecialidades.get(4), 67898, "9:00-17:00", "Dr. Matias Riquelme", "Riquelme", 879654123, 25);
-
-            // Añadir todos los médicos de una vez
-            Collections.addAll(listaMedicos, medico1, medico2, medico3, medico4, medico5, medico6, medico7, medico8, medico9);
-
-            // Inicializar Obras Sociales usando un array y un bucle
-            String[] nombresObrasSociales = {"OSDE", "OSEP", "DAMSU", "Prevencion Salud", "Particular"};
-            for (String nombre : nombresObrasSociales) {
-                listaObraSocial.add(new ObraSocial(nombre));
-            }
-
-            listasCargadas = true; // Marcar que las listas han sido cargadas
+        // Inicializar Especialidades usando un array y un bucle
+        String[] nombresEspecialidades = {"Cardiología", "Pediatría", "Dermatología", "Traumatologia", "Oftalmología"};
+        for (String nombre : nombresEspecialidades) {
+            listaEspecialidades.add(new Especialidades(nombre));
         }
+
+        // Inicializar Médicos
+        Medico medico1 = new Medico(listaEspecialidades.get(0), 12345, "9:00-17:00", "Dr. Juan Pérez", "Pérez", 12345678, 40);
+        Medico medico2 = new Medico(listaEspecialidades.get(1), 67890, "9:00-17:00", "Dra. Maria Sanchez", "Sanchez", 87654321, 35);
+        Medico medico3 = new Medico(listaEspecialidades.get(2), 67890, "9:00-17:00", "Dra. Monica Marquez", "Gómez", 87654321, 31);
+        Medico medico4 = new Medico(listaEspecialidades.get(3), 67890, "9:00-17:00", "Dr. Carlos Arias", "Gómez", 87654321, 31);
+        Medico medico5 = new Medico(listaEspecialidades.get(0), 12345, "9:00-17:00", "Dr. Alfredo Pucci", "Pérez", 12345678, 40);
+        Medico medico6 = new Medico(listaEspecialidades.get(1), 67890, "9:00-17:00", "Dr. Alberto Perez", "Sanchez", 87654321, 35);
+        Medico medico7 = new Medico(listaEspecialidades.get(2), 67890, "9:00-17:00", "Dra. Federica Gomez", "Gómez", 87654321, 31);
+        Medico medico8 = new Medico(listaEspecialidades.get(3), 67890, "9:00-17:00", "Dr. Matias Gonzalez", "Gómez", 87654321, 31);
+        Medico medico9 = new Medico(listaEspecialidades.get(4), 67898, "9:00-17:00", "Dr. Matias Riquelme", "Riquelme", 879654123, 25);
+        
+        listaMedicos.clear();// Limpiar antes de agregar
+        
+        // Añadir todos los médicos de una vez
+        Collections.addAll(listaMedicos, medico1, medico2, medico3, medico4, medico5, medico6, medico7, medico8, medico9);
+
+        // Inicializar Obras Sociales usando un array y un bucle
+        String[] nombresObrasSociales = {"OSDE", "OSEP", "DAMSU", "Prevencion Salud", "Particular"};
+        for (String nombre : nombresObrasSociales) {
+            listaObraSocial.add(new ObraSocial(nombre));
+        }
+
         /*listaMedicos.clear();//Limpia la lista de medico antes de ejecutar el metodo para no tener medicos repetidos
         int cont = 0;
         Random random = new Random();
